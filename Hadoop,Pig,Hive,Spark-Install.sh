5e7411ee1d59ed71ac1e64ff49a4ba9eac63ddbb
Naresh Seegobin <33396127+nseegobin@users.noreply.github.com>
2019-02-09 18:55:50 -0400

    Create Hadoop,Pig,Hive,Spark-Install.sh

---
+++ Hadoop,Pig,Hive,Spark-Install.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+## Program Name:	Download and install Hadoop, Pig, Hive and Spark
+## Compiled by Naresh Seegobin naresh.seegobin@gmail.com
+## Version History:
+## 20190207 1444 - Initial version
+## 20190208 0840 - added https://hadoop.apache.org/
+
+
+## https://hadoop.apache.org/releases.html
+## https://www.apache.org/dyn/closer.cgi/hadoop/common/hadoop-3.1.2/hadoop-3.1.2.tar.gz
+
+## https://hive.apache.org/
+## https://www-us.apache.org/dist/hive/hive-3.1.1/apache-hive-3.1.1-bin.tar.gz
+
+## https://pig.apache.org/
+## https://www-us.apache.org/dist/pig/pig-0.17.0/pig-0.17.0.tar.gz
+
+## https://spark.apache.org/
+## https://www.apache.org/dyn/closer.lua/spark/spark-2.4.0/spark-2.4.0-bin-hadoop2.7.tgz
+
+
+
+mkdir ./Hadoop-et-al
+cd ./Hadoop-et-al
+wget -c https://www.apache.org/dyn/closer.cgi/hadoop/common/hadoop-3.1.2/hadoop-3.1.2.tar.gz
+
+
+wget -c https://www-us.apache.org/dist/hive/hive-3.1.1/apache-hive-3.1.1-bin.tar.gz
+
+
+wget -c https://www-us.apache.org/dist/pig/pig-0.17.0/pig-0.17.0.tar.gz
+
+wget -c https://www.apache.org/dyn/closer.lua/spark/spark-2.4.0/spark-2.4.0-bin-hadoop2.7.tgz
+
