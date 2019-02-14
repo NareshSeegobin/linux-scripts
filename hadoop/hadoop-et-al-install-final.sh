8cc1fad248fe2f7185c0c90736dbf3fdc9d1f58c
Naresh Seegobin <33396127+nseegobin@users.noreply.github.com>
2019-02-14 16:18:45 -0400

    Create hadoop-et-al-install-final.sh

---
+++ hadoop/hadoop-et-al-install-final.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+## Original Program Name:	hadoop-et-al-install-final.sh
+## Compiled by Naresh Seegobin naresh.seegobin@gmail.com
+## Version History:
+## 20190214 1618 - Initial version
+
+
