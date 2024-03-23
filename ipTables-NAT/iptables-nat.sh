c50823c050ad6d1fd8967f78f511e31b9bfe9e73
Naresh Seegobin <33396127+NareshSeegobin@users.noreply.github.com>
2024-03-23 12:16:09 -0400

    Update iptables-nat.sh

--- ipTables-NAT/iptables-nat.sh
+++ ipTables-NAT/iptables-nat.sh
@@ -3,12 +3,29 @@
 echo -n APLICANDO REGLAS...
 
 
+
+
+##[] [STEP 1] - edit /etc/netplan/01-network-manager-all.yaml and add/replace entries below
+
+## ENTRY 1 START
+
 # /etc/netplan/01-network-manager-all.yaml
 # Let NetworkManager manage all devices on this system
 # network:
   # version: 2
   # renderer: NetworkManager
 
+#  20240323 - Added by Naresh Seegobin for General use
+  # ethernets:
+      # wlp1s0:
+        # dhcp4: yes
+      # enx00e04c6804f9:
+        # dhcp4: yes
+          
+
+## ENTRY 1 END
+
+## ENTRY 2 START - SWAP AS REQURED
 #  20231004 - Added by Naresh Seegobin for UWI INEGI
   # ethernets:
       # wlp1s0:
@@ -17,28 +34,52 @@ echo -n APLICANDO REGLAS...
         # addresses:
           # - 10.217.105.1/24
 
+## ENTRY 2 END - SWAP AS REQURED
+
+
+
+
+
+##[] [STEP 2] - execute command below
 
 # netplan apply
 
 
 
 
+
+##[] [STEP 3] - edit /etc/sysctl.conf and add entry below
+
 # /etc/sysctl.conf
 # net.ipv4.ip_forward=1
+
+
+
+
+
+##[] [STEP 4] - execute command below
 
 # sysctl -p
 
 
 
+
+
+##[] [STEP 5] - execute command below
+
 # apt install iptables-persistent
 
 
+
+
+##[] [STEP 6] - START BLOCK - execute command in the block below
 
 # INTERFACES
 ##EXTERNA="enp0s3"
 ##INTERNA="enp0s8"
 
 EXTERNA="wlp1s0"
+## Change Internal depending on your NIC adapter
 INTERNA="enx00e04c6804f9"
 
 
@@ -61,6 +102,7 @@ iptables -A OUTPUT -o lo -j ACCEPT
 
 
 # ALLOW TCP & UDP PROTOCOL & Proxy
+## AND Allow DNS, see https://www.linuxquestions.org/questions/linux-security-4/iptables-dns-not-working-4175673577/
 iptables -t filter -A FORWARD -i $INTERNA -p udp --dport 53 -j ACCEPT
 iptables -t filter -A FORWARD -i $INTERNA -p tcp --dport 53 -j ACCEPT
 iptables -t filter -A FORWARD -m state --state ESTABLISHED,RELATED -j ACCEPT
@@ -99,8 +141,43 @@ echo " "
 iptables-save
 
 
+##[] [STEP 6] - END BLOCK - execute command in the block above
+
+
+
+
+
+
+
+##[] [STEP 7] - execute command in the block below, remove comments
+
 #### sudo su
 #### sudo iptables-save > /etc/iptables/rules.v4
 
 sudo cat /etc/iptables/rules.v4
+
+
+
+
+
+##[] [STEP 8] - END - References
+
+### Reference links
+
+
+## https://www.networkreverse.com/2020/06/how-to-build-linux-router-with-ubuntu.html
+## https://www.linuxquestions.org/questions/linux-security-4/iptables-dns-not-working-4175673577/
+
+
+
+
+
+
+
+
+
+
+
+
+
 
